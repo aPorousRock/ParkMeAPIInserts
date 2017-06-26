1eb17dd59cfbbd8db5edf9ba4683c58400720406
Ajinkya Parkar <Ajinkya.Parkar@ibm.com>
2017-06-26 16:58:48 -0400

    removed extra lines
    removed extra lines

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const routes = require('./routes/routes');
 const cors = require('cors');
-
 const app = express();
 app.use(cors());
 app.use(routes);
-
 app.listen(5000,'0.0.0.0', function() {
   console.log("API Server running at port 5000");
 });

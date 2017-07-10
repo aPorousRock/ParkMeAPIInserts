0d425ae46f120180c98ea7439c148f95298e5e6b
Preeti K Dalvi <Preeti.Dalvi@ibm.com>
2017-07-10 16:59:06 -0400

    changes

--- newapiserver/app/routes/my_routes.js
+++ newapiserver/app/routes/my_routes.js
@@ -3,7 +3,7 @@ var ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db,callback) {
 
   app.get('/getAlertsQuickSummaryData', (req, res) => {
-    db.collection('AlertExp').find({}).toArray(function(err, docs) {
+    db.collection('mockdata').find({}).toArray(function(err, docs) {
       if (err) {
         res.send({'error':'An error has occurred'});
       } else {
@@ -22,7 +22,7 @@ module.exports = function(app, db,callback) {
   //  const completed_at=req.query.completed_at;
   // console.log(account_name);
   // const details = {'adopter_name':adopter_name};
-    db.collection('AlertExp').find({'date':date}).toArray(function (err, items){
+    db.collection('mockdata').find({'date':date}).toArray(function (err, items){
       if (err) {
         res.send({'error':'An error has occurred'});
       } else {
@@ -34,50 +34,50 @@ module.exports = function(app, db,callback) {
       return res.send(items);
     });
   });
-  app.get('/getAlertsQuickSummaryDataByAdopter', (req, res) => {
-    const username= req.query.username;
-  //  const completed_at=req.query.completed_at;
-  // console.log(account_name);
-  // const details = {'adopter_name':adopter_name};
-    db.collection('AlertExp').find({'username':username}).toArray(function (err, items){
-      if (err) {
-        res.send({'error':'An error has occurred'});
-      } else {
-        res.send(items);
-      }
-      console.log("Found the following record");
-    //  callback(items);
-      console.dir(items);
-      return res.send(items);
-    });
-  });
-  app.put('/updateAlertsByAdopter', (req, res) => {
-    const username= req.query.username;
-    const date=req.query.date;
-
-
-  // console.log(account_name);
-  //  const details = {};
-    db.collection('AlertExp').updateOne({"username":"adopter111"},{$set:{"date":"2017-07-30"}},(err,items)=> {
-      if (err) {
-        return res.send({'error':'An error has occurred'});
-      } else {
-        return res.send(items);
-      }
-      console.log("Found the following record");
-     //callback(items);
-   console.dir(items);
-  // return res.write();
-//res.writeContinue();
-//return res.send(items);
-   res.send(items);
-    });
-  });
+  // app.get('/getAlertsQuickSummaryDataByAdopter', (req, res) => {
+  //   const username= req.query.username;
+  // //  const completed_at=req.query.completed_at;
+  // // console.log(account_name);
+  // // const details = {'adopter_name':adopter_name};
+  //   db.collection('mockdata').find({'username':username}).toArray(function (err, items){
+  //     if (err) {
+  //       res.send({'error':'An error has occurred'});
+  //     } else {
+  //       res.send(items);
+  //     }
+  //     console.log("Found the following record");
+  //   //  callback(items);
+  //     console.dir(items);
+  //     return res.send(items);
+  //   });
+  // });
+//   app.put('/updateAlertsByAdopter', (req, res) => {
+//     const username= req.query.username;
+//     const date=req.query.date;
+//
+//
+//   // console.log(account_name);
+//   //  const details = {};
+//     db.collection('mockdata').updateOne({"username":"adopter111"},{$set:{"date":"2017-07-30"}},(err,items)=> {
+//       if (err) {
+//         return res.send({'error':'An error has occurred'});
+//       } else {
+//         return res.send(items);
+//       }
+//       console.log("Found the following record");
+//      //callback(items);
+//    console.dir(items);
+//   // return res.write();
+// //res.writeContinue();
+// //return res.send(items);
+//    res.send(items);
+//     });
+//   });
 
 
   app.post('/postAlertsQuickSummaryData', (req, res) => {
       const note = req.body;
-      db.collection('AlertExp').insert(note, (err, result) => {
+      db.collection('mockdata').insert(note, (err, result) => {
         if (err) {
           res.send({ 'error': 'An error has occurred' });
         } else {

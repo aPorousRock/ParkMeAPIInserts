e143f6d70478df452d10104d9aa67770038413d5
Preeti K. Dalvi <Preeti.Dalvi@ibm.com>
2017-07-14 20:08:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(routes);
+
 
 
 // app.listen(8050,'0.0.0.0', function() {

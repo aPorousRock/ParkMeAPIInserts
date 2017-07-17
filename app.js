9a665086ff8fc2ac47ddded9c415903885fb5762
Preeti K Dalvi <Preeti.Dalvi@ibm.com>
2017-07-17 13:38:38 -0400

    Final changes done

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const routes = require('./routes/routes');
 const cors = require('cors');
+
+const app = express();
 const bodyParser     = require('body-parser');
+
+app.use(bodyParser.json({ extended: true }));
+
+
 const passport = require('passport');
 const http = require('http');
 const io = require('socket.io');
 
-const app = express();
-
-app.use(bodyParser.json({ extended: true }));
 
 const server = http.Server(app);
 const socketIO = io(server);
+
 
 app.use(cors());
 app.use(function(req, res, next){
@@ -24,11 +28,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(routes);
+
+
 // app.listen(8050,'0.0.0.0', function() {
 // console.log("port 5000 running");
 // })
 
-server.listen(8050,'0.0.0.0', function() {
+server.listen(5000,'0.0.0.0', function() {
   console.log("API Server running at port 5000");
 
 });
